feececd4533e8f842b780c1afb863b86908fec76
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:19:21 -0600

    Add files via upload

---
+++ CoalescingOperatorNull/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoalescingOperatorNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? ticketsVendidos = null;
+            int ticketsDisponibles = ticketsVendidos ?? 10;
+
+            //usamoes ?? cuando se trabaja con valores nulos, en el ejemplo de arriba
+            //explica que ticketsDisponibles sera igual a, si ticketsVendidos es un valor null
+            //este automaticamentes se convertira en 0, en caso contrario, tomara el valor 
+            //que tenga asignado la variable ticketsVendidos
+            Console.WriteLine("ticketsDisponibles = {0}", ticketsDisponibles);
+            Console.ReadKey();
+        }
+    }
+}
